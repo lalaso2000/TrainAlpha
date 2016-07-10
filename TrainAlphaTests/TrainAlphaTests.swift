be49a343983992401f848ef620fb70db2d3b07d7
丹羽拓実 <raraso2000@ezweb.ne.jp>
2016-07-10 22:19:35 +0900

    Initial Commit

---
+++ TrainAlphaTests/TrainAlphaTests.swift
@@ -0,0 +1,36 @@
+//
+//  TrainAlphaTests.swift
+//  TrainAlphaTests
+//
+//  Created by 丹羽拓実 on 2016/07/10.
+//  Copyright © 2016年 丹羽拓実. All rights reserved.
+//
+
+import XCTest
+@testable import TrainAlpha
+
+class TrainAlphaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
